9f3790366f375d5e0d58d43bdc588c7c9a7fa074
VINIT-PAT <144584021+VINIT-PAT@users.noreply.github.com>
2024-12-05 18:52:28 +0530

    Frontend-Pointer

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', // Adjust as needed for your frontend URL
+  origin: 'https://vinit-pat.github.io/Assess-FE/', // Adjust as needed for your frontend URL
 }));
 
 // Connect to MongoDB
